9304f8637cf59748c5f752b88b392738025f9a02
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-08-11 00:20:31 +0400

    layer-lar elave edildi ve elaqelendirildi.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    // IEntity implement eden class bir veritabani tablosudur.
+    public interface IEntity
+    {
+
+    }
+}
