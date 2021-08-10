7b30740cbbb21bb64a79001c9a3352ee78da47ae
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-08-11 03:02:28 +0400

    Entity Framework tetbiq edildi.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    // generic constraint
+    // class - reference type
+    // IEntity - IEntity ola bilir ve ya IEntity implement eden class ola bilir.
+    // new() - new'lene bilir olmali.
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
