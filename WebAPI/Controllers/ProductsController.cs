d8887b8dcb06b62958bb4eda399e7e9f6b7770af
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-08-30 15:06:38 +0400

    businnes layer-daki iproductservice-in api qarsiligi yazildi.

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,14 +15,17 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        // Loosely coupled
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
         [HttpGet]
         public List<Product> Get()
         {
-            return new List<Product>
-            {
-                new Product{ProductId=1,ProductName="Elma"},
-                new Product{ProductId=2,ProductName="Armut"}
-            };
+            var result = _productService.GetAll();
+            return result.Data;
         }
     }
 }
