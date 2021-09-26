1cfc1d35aeb87e6390c40cbc33d221c18d7af720
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-09-26 23:42:50 +0400

    jwt elave edildi

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -10,6 +10,8 @@ using System.Collections.Generic;
 using System.Text;
 using Castle.DynamicProxy;
 using Business.CCS;
+using Core.Utilities.Security.JWT;
+using Microsoft.AspNetCore.Http;
 
 namespace Business.DependencyResolvers.Autofac
 {
@@ -22,6 +24,14 @@ namespace Business.DependencyResolvers.Autofac
 
             builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
             builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+
+
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
